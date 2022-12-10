346eab3f4eb42c0401ebbdc6f1bf797b2d0ac26e
Vikashkumar Choudhary <106760576+vikashkumar-19@users.noreply.github.com>
2022-12-11 03:56:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "@szhsin/react-menu/dist/index.css";
+import "@szhsin/react-menu/dist/transitions/slide.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import {TableComponent} from "./Component/Table";
+import "./App.css"
+
+function App() {
+  return (
+    <div className="text-center">
+    
+      <TableComponent />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Table.jsx
@@ -0,0 +1,184 @@
+import React, { useState, useEffect } from "react";
+import Papa from "papaparse";
+import tableDataCSV from "../Data/tableData.csv";
+import { ThreeDotsVertical } from "react-bootstrap-icons";
+import { Menu, MenuItem, MenuButton } from "@szhsin/react-menu";
+
+function StringToInt(str) {
+  return parseInt(str, 10);
+}
+
+function sortTable(column, type) {
+  if (column === "id") {
+    if (type === "ASC") {
+      return (a, b) =>
+        StringToInt(a[column]) > StringToInt(b[column]) ? 1 : -1;
+    } else if (type === "DESC") {
+      return (a, b) =>
+        StringToInt(b[column]) < StringToInt(a[column]) ? -1 : 1;
+    }
+    return (a, b) => 1;
+  }
+  if (type === "ASC") {
+    return (a, b) => (a[column] > b[column] ? 1 : -1);
+  } else if (type === "DESC") {
+    return (a, b) => (b[column] < a[column] ? -1 : 1);
+  }
+  return (a, b) => 1;
+}
+
+export function TableComponent(props = {}) {
+  const [CSVData, setCSVData] = useState([]);
+  const [currentSort, setCurrentSort] = useState("default");
+  const [currentColumn, setCurrentColumn] = useState("default");
+  const [activeRow, setactiveRow] =  useState(new Map());; 
+  
+  var titles = [
+    "id",
+    "first_name",
+    "last_name",
+    "email",
+    "gender",
+    "ip_address",
+    "airport code",
+    "time",
+    "status",
+    "mobile",
+    "area",
+    "show",
+    "edit",
+  ];
+
+  function onSortChange(col, type) {
+    setCurrentSort(type);
+    setCurrentColumn(col);
+  }
+
+  function parseCSVData() {
+    Papa.parse(tableDataCSV, {
+      header: true,
+      download: true,
+      complete: (result) => {
+        setCSVData(result.data);
+      },
+    });
+  }
+
+  function changeRowColor(row){
+	let cur = activeRow.get(row);
+	cur = (cur+1)%2;
+	setactiveRow(activeRow.set(row,cur));
+  }
+  useEffect(() => {
+    parseCSVData();
+	for(var i=0;i<CSVData.length;i++){
+		setactiveRow(activeRow.set(CSVData[i].id,0));
+	}
+  }, []);
+  
+  console.log(currentColumn, currentSort);
+  return (
+    <table className="table">
+      <thead className="">
+        <tr>
+          {titles.map(function (obj, ind) {
+            return (
+              <>
+                <th className="d-md-table-cell" key={ind}>
+                  <div className="flex">
+                    {obj}
+                    <Menu
+                      menuButton={
+						<button className="threedots">  <ThreeDotsVertical></ThreeDotsVertical> </button>
+                          
+                      }
+                      transition
+                    >
+                      <MenuItem
+                        onClick={() => {
+                          onSortChange("default", "default");
+                        }}
+                        disabled={currentSort === "default"}
+                      >
+                        UnSort
+                      </MenuItem>
+                      <MenuItem
+                        onClick={() => {
+                          onSortChange(obj, "ASC");
+                        }}
+                        disabled={
+                          currentSort === "ASC" && currentColumn === obj
+                        }
+                      >
+                        Sort by ASC
+                      </MenuItem>
+                      <MenuItem
+                        onClick={() => {
+                          onSortChange(obj, "DESC");
+                        }}
+                        disabled={
+                          currentSort === "DESC" && currentColumn === obj
+                        }
+                      >
+                        Sort by DeSC
+                      </MenuItem>
+                    </Menu>
+                  </div>
+                </th>
+              </>
+            );
+          })}
+        </tr>
+      </thead>
+      <tbody>
+        {[...CSVData]
+          .sort(sortTable(currentColumn, currentSort))
+          .map(function (obj, ind) {
+            return <RowComponent data={obj} key={ind} active={(activeRow.get(obj.id)===1)} onClick={()=>{changeRowColor(obj.id)}}/>;
+          })}
+      </tbody>
+    </table>
+  );
+}
+
+function RowComponent(props = {}) {
+  return (
+    <tr className={props.active? "need-color-change":""}>
+      {/* <td className="d-md-none d-table-cell">
+        <div className="card">
+          <div className="card-body">
+            <strong className="card-title">{props.description}</strong>
+            <p className="card-text">
+              UPC {props.upc}
+              <br />
+              {props.isn && `ISN ${props.isn}`}
+            </p>
+            <button className="btn btn-secondary btn-block">Action</button>
+          </div>
+        </div>
+      </td> */}
+      <td className=" d-md-table-cell">{props.data.id}</td>
+      <td className="d-md-table-cell">{props.data.first_name}</td>
+      <td className="d-md-table-cell">{props.data.last_name}</td>
+      <td className="d-md-table-cell">{props.data.email}</td>
+      <td className=" d-md-table-cell">{props.data.gender}</td>
+      <td className=" d-md-table-cell">{props.data.ip_address}</td>
+      <td className=" d-md-table-cell">{props.data["airport code"]}</td>
+      <td className=" d-md-table-cell">{props.data.time}</td>
+      {props.data.status === "true" && (
+        <td className="d-none d-md-table-cell" style={{ color: "green" }}>
+          {props.data.status}
+        </td>
+      )}
+      {props.data.status === "false" && (
+        <td className="d-none d-md-table-cell" style={{ color: "red" }}>
+          {props.data.status}
+        </td>
+      )}
+      <td className="d-none d-md-table-cell">{props.data.mobile}</td>
+      <td className="d-none d-md-table-cell">{props.data.area}</td>
+      <td className="d-none d-md-table-cell">{props.data.show}</td>
+      <td className="d-none d-md-table-cell">{props.data.edit}</td>
+    </tr>
+  );
+}
